8ffe4f07415cce9cc9d1846ff13b67da4487f4b7
Hadiwasito1905 <noverryambo@gmail.com>
2021-12-14 16:55:33 +0700

    database connect and rename entity

--- src/main/java/com/sinaukoding/perpustakaan/entity/BaseEntity.java
+++ src/main/java/com/sinaukoding/perpustakaan/entity/BaseEntity.java
@@ -1,22 +1,21 @@
-package com.sinaukoding.perpustakaan.Entity;
+package com.sinaukoding.perpustakaan.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.DynamicUpdate;
 
-import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @DynamicUpdate
-@SuppressWarnings('unchecked')
+@SuppressWarnings("unchecked")
 
 
 
-public abstract class baseEntity<I> implements Serializable {
+public abstract class BaseEntity<I> implements Serializable {
 
-
+    private static final long serialVersionUID = -1477876135846170795L;
 }
